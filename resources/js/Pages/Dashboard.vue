d21907b17beb487773162b19558de8b1c77e97f5
Fernando <fernandomadethat@gmail.com>
2024-08-13 23:07:57 -0300

    feature: appling theme to frontend

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,13 +7,11 @@ import CandleStickChart from '@/Components/CandleStickChart.vue';
 <template>
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl leading-tight text-white">
-                Dashboard
-            </h2>
+
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="py-4 px-4">
+            <div class=" sm:px-6 lg:px-1">
                 <div style="height: 500px;" class=" flex items-center justify-center shadow-lg text-white bg-gradient-to-r from-sky-950 to-indigo-950 overflow-hidden sm:rounded-lg">
                         WHITE SPACE
                 </div>
